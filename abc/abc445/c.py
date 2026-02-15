118e219efa53bb842a1fff76d8ad787d84fcb8e7
gigo <431731+gigo-gigo@users.noreply.github.com>
2026-02-15 19:23:36 +0900

    Refactor solve function to simplify logic and remove unused SCCGraph implementation

--- abc/abc445/c.py
+++ abc/abc445/c.py
@@ -1,33 +1,8 @@
-from atcoder.scc import SCCGraph
-
-
 def solve(N, A):
     A = [0] + A
-
-    B = [-1] * (N + 1)
-    for i, a in enumerate(A):
-        B[a] = i
-
-    G = SCCGraph(N + 1)
-    for i, a in enumerate(A):
-        G.add_edge(i, a)
-
     X = [-1] * (N + 1)
-    for group in G.scc()[::-1]:
-        if len(group) > 1 or len(group) == 1 and group[0] == A[group[0]]:
-            u = group[0]
-            i = pow(10, 100, len(group))
-            v = group[i]
-            X[u] = v
-            while X[A[u]] == -1:
-                X[A[u]] = A[v]
-                u, v = A[u], A[v]
-        else:
-            u = group[0]
-            v = A[u]
-            vv = X[v]
-            uu = B[vv]
-            X[u] = X[uu]
+    for u in range(N, 0, -1):
+        X[u] = A[u] if u == A[u] else X[A[u]]
 
     return X[1:]
 
